16a6ffd76e453d48b15560d84286bdced12b0587
João G. Salgado <jgs1884@gmail.com>
2020-01-23 23:01:17 -0300

    inicial

---
+++ mobile/src/pages/Profile.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Webview } from 'react-native-webview';
+
+function Profile({ navigation })  {
+    const githubUsername = navigation.getParam('github_username'); 
+    return (
+        <Webview style={{ flex: 1 }} source={{ uri: `https://github.com/${githubUsername}` }} /> 
+    )
+}
+
+export default Profile;
